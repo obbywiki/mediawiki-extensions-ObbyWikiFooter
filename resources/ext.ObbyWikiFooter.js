b5d73d453827ee96261c95094b90aee5c2854673
Wolfite <100052562+wlft@users.noreply.github.com>
2026-03-11 22:52:00 +1000

    polyfill for extension

--- resources/ext.ObbyWikiFooter.js
+++ resources/ext.ObbyWikiFooter.js
@@ -1,1 +1,9 @@
-// scripts here
+$( function () {
+	// polyfill
+	var $footer = $( '.obbywiki-footer' );
+	var $relatedPosts = $( '.discourse-related-posts' );
+
+	if ( $footer.length && $relatedPosts.length ) {
+		$footer.insertAfter( $relatedPosts.last() );
+	}
+} );
